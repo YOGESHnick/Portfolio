69cdd9f6a710fb909da3cfb9fb2534dff3a2db7e
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-07-08 10:12:56 +0530

    s

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,14 +8,14 @@ function Footer()
     return (
         <div>
             <footer>
-            <div class="bottom-container"> 
-            <a class="footer-link " href="https://www.linkedin.com/in/yogesh-shamlin-shinanth-j-s-6ab98b237/"> <i class="fa-brands fa-linkedin fa-xl"></i> </a>
-            <a class="footer-link " href="https://github.com/YOGESHnick"> <i class="fa-brands fa-github fa-xl"></i> </a>
-            <a class="footer-link " href="./Yogesh Shamlin Shinanth JS _RESUME copy.pdf" target="_blank">Resume  <i class="fa-solid fa-download fa-xl"></i></a>
+            <div className="bottom-container"> 
+            <a className="footer-link " href="https://www.linkedin.com/in/yogesh-shamlin-shinanth-j-s-6ab98b237/"> <i class="fa-brands fa-linkedin fa-2xl"></i> </a>
+            <a className="footer-link " href="https://github.com/YOGESHnick"> <i class="fa-brands fa-github fa-2xl"></i> </a>
+            <a className="footer-link " href="./Yogesh Shamlin Shinanth JS _RESUME copy.pdf" target="_blank">Resume  <i class="fa-solid fa-download fa-2xl"></i></a>
             {/* <a class="footer-link " href="https://earth.google.com/web/search/33.80065299518774,+-114.53205404369159/@33.80066884,-114.53204689,108.45890353a,247.72060588d,35y,0.00000001h,44.4010633t,360r/data=CmoaQBI6Gd44Kcx75kBAIS5tOCwNolzAKiYzMy44MDA2NTI5OTUxODc3NCwgLTExNC41MzIwNTQwNDM2OTE1ORgBIAEiJgokCXRYuScHUUBAEcBx76bHqjlAGZkBD7kGsFjAIZfnp0hVelzA">Location</a> */}
             <br></br>
             <br></br>
-            <p class="cpyrt">©   S H A M L I N    @2023 </p>
+            <p className="cpyrt">©   S H A M L I N    @2023 </p>
 
             </div>
             </footer>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 function Form(){
     return (
-        <div className="form">
-    
-        <form action="https://formsubmit.co/yogeshshamlin@gmail.com" method="POST">
-            <h3>Ping Me!</h3>
-            <input type="text" id="name" name="name" placeholder="Your Name" required /> 
-            <input type="email" id="email" name="email" placeholder="Your Email" required />
-            <input type="text" id="phone" name="phone" placeholder="Phone no." required />
-            <textarea id="message" rows="4" name="Type your message" placeholder="Type your message"></textarea>
-            <button type="submit">Send</button>
-        </form>
+        <div className="row form">
+            <div className="col-sm-12 col-lg-6">
+                <img className="form-img" src="coat_suit-fotor-20230619204039.png"></img>
+            </div>
 
+            <div className="col-sm-12 col-lg-6">
+                <form action="https://formsubmit.co/yogeshshamlin@gmail.com" method="POST">
+                    {/* <!-- Honeypot --> */}
+                    {/* <input type="text" name="_honey" style="display:none;"></input> */}
+
+                    {/* <!-- Disable Captcha --> */}
+                    <input type="hidden" name="_captcha" value="false"></input>
+
+                    <h3>Ping Me!</h3>
+                    <input type="text" id="name" name="name" placeholder="Your Name" required></input>
+                    <input type="email" id="email" name="email" placeholder="Your Email" required></input>
+                    <input type="text" id="phone" name="phone" placeholder="Phone no." required></input>
+                    <textarea id="message" rows="4" name="Type your message" placeholder="Type your message"></textarea>
+                    <button type="submit"> <i class="fa-solid fa-shuttle-space fa-bounce fa-xl"></i>Send</button>
+                </form>
+            </div> 
+    
         </div>
     )
 }
