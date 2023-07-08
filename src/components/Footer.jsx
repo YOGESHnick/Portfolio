8b71269ebad099006a80bd636d74c45de772f352
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-07-08 10:16:46 +0530

    ss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer()
             <div className="bottom-container"> 
             <a className="footer-link " href="https://www.linkedin.com/in/yogesh-shamlin-shinanth-j-s-6ab98b237/"> <i class="fa-brands fa-linkedin fa-2xl"></i> </a>
             <a className="footer-link " href="https://github.com/YOGESHnick"> <i class="fa-brands fa-github fa-2xl"></i> </a>
-            <a className="footer-link " href="./Yogesh Shamlin Shinanth JS _RESUME copy.pdf" target="_blank">Resume  <i class="fa-solid fa-download fa-2xl"></i></a>
+            <a className="footer-link " href="./public/YOGESH_SHAMLIN_SHINANTH-RESUME copy.pdf" target="_blank">Resume  <i class="fa-solid fa-download fa-2xl"></i></a>
             {/* <a class="footer-link " href="https://earth.google.com/web/search/33.80065299518774,+-114.53205404369159/@33.80066884,-114.53204689,108.45890353a,247.72060588d,35y,0.00000001h,44.4010633t,360r/data=CmoaQBI6Gd44Kcx75kBAIS5tOCwNolzAKiYzMy44MDA2NTI5OTUxODc3NCwgLTExNC41MzIwNTQwNDM2OTE1ORgBIAEiJgokCXRYuScHUUBAEcBx76bHqjlAGZkBD7kGsFjAIZfnp0hVelzA">Location</a> */}
             <br></br>
             <br></br>
