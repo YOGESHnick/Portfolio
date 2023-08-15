b58dd990ac1cda906b0de1eb198e8028bbf54a26
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-15 12:52:57 +0530

    back with a bang !

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 
 function Form(){
     return (
-        <div className="row form">
-            <div className="col-sm-12 col-lg-6">
-                <img className="form-img" src="coat_suit-fotor-20230619204039.png"></img>
-            </div>
+        <section id="form">
+            <div className="row form">
+                <div className="col-sm-12 col-lg-6">
+                    <img className="form-img" src="coat_suit-fotor-20230619204039.png"></img>
+                </div>
 
-            <div className="col-sm-12 col-lg-6">
-                <form action="https://formsubmit.co/yogeshshamlin@gmail.com" method="POST">
+                <div className="col-sm-12 col-lg-6">
+                    <form action="https://formsubmit.co/yogeshshamlin@gmail.com" method="POST">
                     {/* <!-- Honeypot --> */}
                     {/* <input type="text" name="_honey" style="display:none;"></input> */}
 
                     {/* <!-- Disable Captcha --> */}
-                    <input type="hidden" name="_captcha" value="false"></input>
+                        <input type="hidden" name="_captcha" value="false"></input>
 
-                    <h3>Ping Me!</h3>
-                    <input type="text" id="name" name="name" placeholder="Your Name" required></input>
-                    <input type="email" id="email" name="email" placeholder="Your Email" required></input>
-                    <input type="text" id="phone" name="phone" placeholder="Phone no." required></input>
-                    <textarea id="message" rows="4" name="Type your message" placeholder="Type your message"></textarea>
-                    <button type="submit"> <i class="fa-solid fa-shuttle-space fa-bounce fa-xl"></i>Send</button>
-                </form>
-            </div> 
-    
-        </div>
+                        <h3>Ping Me!</h3>
+                        <input type="text" id="name" name="name" placeholder="Your Name" required></input>
+                        <input type="email" id="email" name="email" placeholder="Your Email" required></input>
+                        <input type="text" id="phone" name="phone" placeholder="Phone no." required></input>
+                        <textarea id="message" rows="4" name="Type your message" placeholder="Type your message"></textarea>
+                        <button type="submit"> <i class="fa-solid fa-shuttle-space fa-bounce fa-xl"></i>Send</button>
+                    </form>
+                </div>
+            </div>
+        </section>    
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,31 +5,35 @@ function Header()
 {
     return(
         <div>
-            <nav className="navbar navbar-expand-lg mnc py-lg-4 fixed-top">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">S H A M L I N</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto px-2 mx-auto">
-                            <li className="nav-item px-2">
-                                <a className="nav-link " aria-current="page" href="index.html">Home</a>
-                            </li>
-                            <li className="nav-item px-2">
-                              <a className="nav-link" href="skills.html">Skills</a>
-                            </li>
-                            <li className="nav-item px-2">
-                              <a class="nav-link" href="#">Projects</a>
-                            </li>
-                            <li className="nav-item px-2">
-                              <a className="nav-link" href="#">Education</a>
-                            </li>
-                            
-                        </ul>
+            <section id ="header" >
+                <nav className="navbar navbar-expand-lg mnc py-lg-4 fixed-top">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">S H A M L I N</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto px-2 mx-auto">
+                                <li className="nav-item px-2">
+                                    <a className="nav-link " aria-current="page" href="#header">Home</a>
+                                </li>
+                                <li className="nav-item px-2">
+                                    <a className="nav-link" href="#skills">Skills</a>
+                                </li>
+                                <li className="nav-item px-2">
+                                    <a class="nav-link" href="#projects">Projects</a>
+                                </li>
+                                <li className="nav-item px-2">
+                                    <a className="nav-link" href="#">Education</a>
+                                </li>
+                                <li className="nav-item px-2">
+                                    <a className="nav-link" href="#form">Contact</a>
+                                </li> 
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </section>
         </div>
     );
 };
