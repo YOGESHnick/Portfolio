4004f329ee4c7115e4474b288cddbf3e1f2982ff
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-17 21:07:55 +0530

    spinup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import "./app.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return;
+  <div>
+    <section id="Homepage">Hero</section>
+    <section id="Services">Parallax</section>
+    <section>Services</section>
+    <section id="Portfolio">Parallax</section>
+    <section id="Contact">Portfolio 1</section>
+    <section id="Contact">Portfolio 2</section>
+    <section id="Contact">Portfolio 3</section>
+    <section id="Contact">Contact</section>
+  </div>;
 };
 
 export default App;
