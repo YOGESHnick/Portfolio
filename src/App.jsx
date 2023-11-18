0fde8f864623634f8efd5160c96c65359fee6d21
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-18 21:30:44 +0530

    1st page done!

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./app.scss";
 // import Cursor from "./components/cursor/Cursor";
 // import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 // import Parallax from "./components/parallax/Parallax";
 // import Portfolio from "./components/portfolio/Portfolio";
 // import Services from "./components/services/Services";
@@ -13,6 +14,7 @@ const App = () => {
     <div>
       <section id="Home">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services" >Parallax</section>
       <section>Services</section>
