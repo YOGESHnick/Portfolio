d651f943b40f49aa12f220016d4d8b7a61386aeb
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-18 18:28:27 +0530

    button modifed

--- src/components/sidebar/toggleButton/Togglebutton.jsx
+++ src/components/sidebar/toggleButton/Togglebutton.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { motion } from "framer-motion";
 
-export default function Togglebutton({setOpen }) {
+export default function Togglebutton({ setOpen }) {
   return (
-    <button onClick={ ()=> setOpen( (prev)=> !prev ) } >
-      button
+    <button onClick={() => setOpen((prev) => !prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { opacity: 1, d: "M 2 8.923 L 20 8.923" }, // Adjust the initial 'd' attribute
+            open: { opacity: 0 },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 16.346 L 20 16.346" },
+            open: { d: "M 3 2.5 L 17 16.346" },
+          }}
+        />
+      </svg>
     </button>
   );
 }
