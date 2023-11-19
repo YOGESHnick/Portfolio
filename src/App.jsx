c6daaad6f123f538d5adcd318354299e509b1157
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-19 08:15:54 +0530

    adding space motions !

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./app.scss";
 // import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
 // import Parallax from "./components/parallax/Parallax";
 // import Portfolio from "./components/portfolio/Portfolio";
 // import Services from "./components/services/Services";
@@ -16,10 +17,10 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services" >Parallax</section>
+      <section id="Services" ><Parallax type="services" /></section>
       <section>Services</section>
+      <section id="Portfolio" ><Parallax type="portfolio" /> 1</section>
       <section >Parallax</section>
-      <section id="Portfolio" >Portfolio 1</section>
       <section >Portfolio 2</section>
       <section id="Contact">Portfolio 3</section>
       <section id="About" >Contact</section>
