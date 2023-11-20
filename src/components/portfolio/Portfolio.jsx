588b6817e5b073c079ea5edb585297db3b0c700d
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-20 17:40:22 +0530

    new feature

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -30,10 +30,33 @@ const items = [
 ];
 
 const Single = ({ item }) => {
-  return <section>{item.title}</section>;
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default function Portfolio() {
+const Portfolio = () => {
   const ref = useRef();
 
   const { scrollYProgress } = useScroll({
@@ -45,8 +68,9 @@ export default function Portfolio() {
     stiffness: 100,
     damping: 30,
   });
+
   return (
-    <div className="portfolio" ref={ref} >
+    <div className="portfolio" ref={ref}>
       <div className="progress">
         <h1>Featured Works</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
@@ -56,4 +80,6 @@ export default function Portfolio() {
       ))}
     </div>
   );
-}
+};
+
+export default Portfolio;
